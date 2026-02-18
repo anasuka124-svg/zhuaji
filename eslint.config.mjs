c7ae1b1279fa617eef9ded8772bd0528bf63d8c5
安 <anasuka124@gmail.com>
2026-02-18 20:45:01 +0800

    Create eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "react-hooks/exhaustive-deps": "off",
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-img-element": "off",
+  },
+}, {
+  ignores: ["node_modules/**", ".next/**"]
+}];
+
+export default eslintConfig;
